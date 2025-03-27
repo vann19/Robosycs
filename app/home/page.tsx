255d5a1edb661a3d2742959265deb3ee6218188c
vann19 <ahmadfansya660@gmail.com>
2025-03-27 10:41:13 +0700

    start

---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../Components/Navbar';
+const Home = () => {
+  return (
+    <div className='items-center justify-center latar'>
+      < Navbar />
+    </div>
+  )
+}
+
+export default Home
---
+++ app/lomba/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LombaPage = () => {
+  return (
+    <div>LombaPage</div>
+  )
+}
+
+export default LombaPage
